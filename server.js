8851d99b012ada161ed0236f67aca0578ab856db
Rick Steinberger <rick.steinberger@icloud.com>
2019-12-26 20:29:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -146,15 +146,20 @@ function dedupe(ary, field){
 
 //***********************************
 
+// mongoCreateCollections();
 
 // locationRegions();
 // locationCountries();
 // locationStates();
+// locationLocations();
+// locationDulles();
 
-// mongoCreateCollections();
 // locationRegionInserts();
 // locationCountryInserts();
-locationStateInserts();
+// locationStateInserts();
+// locationLocationInserts();
+locationDullesInserts();
+
 
 
 //***********************************
@@ -176,6 +181,16 @@ function locationStateInserts() {
   mongoInsert(obj);
 }
 
+function locationLocationInserts() {
+  var obj = JSON.parse(fs.readFileSync('/Users/ricksteinberger/Home/git/mongo-data/location-locations.json', 'utf8'));
+  mongoInsert(obj);
+}
+
+function locationDullesInserts() {
+  var obj = JSON.parse(fs.readFileSync('/Users/ricksteinberger/Home/git/mongo-data/location-dulles.json', 'utf8'));
+  mongoInsert(obj);
+}
+
 
 //***********************************
 
@@ -188,7 +203,7 @@ function locationRegions() {
 
   var results = [];
   locations.forEach((element, index) => { 
-    var obj = {"_id": uuidv4(), "_class": "region", "name": element.region };
+    var obj = {"_id": uuidv4(), "_class": "region", "name": element.region, "has_active": false };
     results.push(obj);
   });
   writeFile(outFile, results);
@@ -204,7 +219,7 @@ function locationCountries() {
 
   var result = [];
   locations.forEach((element, index) => { 
-    var obj = {"_id": uuidv4(), "_class": "country", "name": element.country };
+    var obj = {"_id": uuidv4(), "_class": "country", "name": element.country, "has_active": false };
 
       locationRegions.forEach((element1, index) => { 
         if( element.region == element1.name ) {
@@ -239,7 +254,7 @@ function locationStates() {
       temp = temp.replace(" ", "");      
     }
 
-    var obj = {"_id": uuidv4(), "_class": "province", "name": temp };
+    var obj = {"_id": uuidv4(), "_class": "province", "name": temp, "has_active": false };
 
       locationCountries.forEach((element1, index) => { 
         if( element.country == element1.name ) {
@@ -263,6 +278,443 @@ function locationStates() {
   });
   writeFile(outFile, result);
 }
+
+function locationLocations() {
+  var locationStates = JSON.parse(fs.readFileSync('/Users/ricksteinberger/Home/git/mongo-data/location-states.json', 'utf8'));  
+  var locations = JSON.parse(fs.readFileSync('/Users/ricksteinberger/Home/git/mongo-data/locations.json', 'utf8'));  
+  var outFile = '/Users/ricksteinberger/Home/git/mongo-data/location-locations.json';
+  deleteFile(outFile);
+
+  var results = [];
+  var BreakException = {};
+  var ctr = 0;
+
+  locations.forEach((element, index) => { 
+
+    try {
+      locationStates.forEach((element1, index1) => { 
+        var location = {};
+        ctr++;
+        if( element.state == element1.name ) {
+          location._id = uuidv4();
+          location._class = "location";
+          location.name = element.name;
+          location.location_code = element.code;
+          location.latitude = element.lat;
+          location.longitude = element.lon;
+          location.city_name = element.city;
+          location.province_name = element.state;
+          location.country_name = element.country;
+          location.country_code = element.country_code;
+          location.region_name = element.region;
+          location.region_id = element1.region_id;
+          location.country_id = element1.country_id;
+          location.province_id = element1._id;
+          location.has_active = false;
+          results.push(location);
+          throw BreakException;
+        }
+      });
+
+    } catch( e ) {
+      console.log('break after ' + ctr);
+      ctr = 0;
+    }
+
+  });
+  writeFile(outFile, results);
+}
+
+
+
+function locationDulles() {
+  var locations = JSON.parse(fs.readFileSync('/Users/ricksteinberger/Home/git/mongo-data/location-locations.json', 'utf8'));  
+  var outFile = '/Users/ricksteinberger/Home/git/mongo-data/location-dulles.json';
+  deleteFile(outFile);
+
+  var results = [];
+  var BreakException = {};
+  var ctr = 0;
+
+  var counters = {"area_number":1000, "cell_number":2000, "sensor_number":3000};
+
+  locations.forEach((element, index) => { 
+
+    // try {
+
+        ctr++;
+
+
+        if( element.location_code == "IAD" ) {
+
+          var site = {};
+          site._class = "site";
+          site.region_id = element.region_id;
+          site.country_id = element.country_id;
+          site.province_id = element.province_id;
+          site.location_id = element._id;
+          site.has_active = false;
+
+          site._id = uuidv4();
+          site.latitude = element.lat;
+          site.longitude = element.lon;
+          site.name = "Main Terminal";
+          results.push(site);
+
+
+              var area = {};
+              area._class = "area";
+              area.site_id = site._id;          
+              area.region_id = site.region_id;
+              area.country_id = site.country_id;
+              area.province_id = site.province_id;
+              area.location_id = site.location_id;
+              area.latitude = site.latitude;
+              area.longitude = site.longitude;
+              area.has_active = false;
+
+              area._id = uuidv4();
+              area.number = counters.area_number++;
+              area.name = "Main Terminal North Level 1";
+              results.push(area);
+              results = getCells(area, results, counters);
+
+              var area = {};
+              area._class = "area";
+              area.site_id = site._id;          
+              area.region_id = site.region_id;
+              area.country_id = site.country_id;
+              area.province_id = site.province_id;
+              area.location_id = site.location_id;
+              area.latitude = site.latitude;
+              area.longitude = site.longitude;
+              area.has_active = false;
+
+              area._id = uuidv4();
+              area.number = counters.area_number++;
+              area.name = "Main Terminal South Level 1";
+              results.push(area);
+              results = getCells(area, results, counters);
+
+              var area = {};
+              area._class = "area";
+              area.site_id = site._id;          
+              area.region_id = site.region_id;
+              area.country_id = site.country_id;
+              area.province_id = site.province_id;
+              area.location_id = site.location_id;
+              area.latitude = site.latitude;
+              area.longitude = site.longitude;
+              area.has_active = false;
+
+              area._id = uuidv4();
+              area.number = counters.area_number++;
+              area.name = "Main Terminal South Level 2";
+              results.push(area);
+              results = getCells(area, results, counters);
+
+          var site = {};
+          site._class = "site";
+          site.region_id = element.region_id;
+          site.country_id = element.country_id;
+          site.province_id = element.province_id;
+          site.location_id = element._id;
+          site.has_active = false;
+
+          site._id = uuidv4();
+          site.latitude = element.lat;
+          site.longitude = element.lon;
+          site.name = "Concourse A";
+          results.push(site);
+
+
+              var area = {};
+              area._class = "area";
+              area.site_id = site._id;          
+              area.region_id = site.region_id;
+              area.country_id = site.country_id;
+              area.province_id = site.province_id;
+              area.location_id = site.location_id;
+              area.latitude = site.latitude;
+              area.longitude = site.longitude;
+              area.has_active = false;
+
+              area._id = uuidv4();
+              area.number = counters.area_number++;
+              area.name = "Concourse A North";
+              results.push(area);
+              results = getCells(area, results, counters);
+
+              var area = {};
+              area._class = "area";
+              area.site_id = site._id;          
+              area.region_id = site.region_id;
+              area.country_id = site.country_id;
+              area.province_id = site.province_id;
+              area.location_id = site.location_id;
+              area.latitude = site.latitude;
+              area.longitude = site.longitude;
+              area.has_active = false;
+
+              area._id = uuidv4();
+              area.number = counters.area_number++;
+              area.name = "Concourse A South";
+              results.push(area);
+              results = getCells(area, results, counters);
+
+          var site = {};
+          site._class = "site";
+          site.region_id = element.region_id;
+          site.country_id = element.country_id;
+          site.province_id = element.province_id;
+          site.location_id = element._id;
+          site.has_active = false;
+
+          site._id = uuidv4();
+          site.latitude = element.lat;
+          site.longitude = element.lon;
+          site.name = "Concourse B";
+          results.push(site);
+
+              var area = {};
+              area._class = "area";
+              area.site_id = site._id;          
+              area.region_id = site.region_id;
+              area.country_id = site.country_id;
+              area.province_id = site.province_id;
+              area.location_id = site.location_id;
+              area.latitude = site.latitude;
+              area.longitude = site.longitude;
+              area.has_active = false;
+
+              area._id = uuidv4();
+              area.number = counters.area_number++;
+              area.name = "Crew Lounge";
+              results.push(area);
+              results = getCells(area, results, counters);
+
+              var area = {};
+              area._class = "area";
+              area.site_id = site._id;          
+              area.region_id = site.region_id;
+              area.country_id = site.country_id;
+              area.province_id = site.province_id;
+              area.location_id = site.location_id;
+              area.latitude = site.latitude;
+              area.longitude = site.longitude;
+              area.has_active = false;
+
+              area._id = uuidv4();
+              area.number = counters.area_number++;
+              area.name = "Concourse B West";
+              results.push(area);
+              results = getCells(area, results, counters);
+
+              var area = {};
+              area._class = "area";
+              area.site_id = site._id;          
+              area.region_id = site.region_id;
+              area.country_id = site.country_id;
+              area.province_id = site.province_id;
+              area.location_id = site.location_id;
+              area.latitude = site.latitude;
+              area.longitude = site.longitude;
+              area.has_active = false;
+
+              area._id = uuidv4();
+              area.number = counters.area_number++;
+              area.name = "Concourse B South";
+              results.push(area);
+              results = getCells(area, results, counters);
+
+          var site = {};
+          site._class = "site";
+          site.region_id = element.region_id;
+          site.country_id = element.country_id;
+          site.province_id = element.province_id;
+          site.location_id = element._id;
+          site.has_active = false;
+
+          site._id = uuidv4();
+          site.latitude = element.lat;
+          site.longitude = element.lon;
+          site.name = "Concourse C";
+          results.push(site);
+
+              var area = {};
+              area._class = "area";
+              area.site_id = site._id;          
+              area.region_id = site.region_id;
+              area.country_id = site.country_id;
+              area.province_id = site.province_id;
+              area.location_id = site.location_id;
+              area.latitude = site.latitude;
+              area.longitude = site.longitude;
+              area.has_active = false;
+
+              area._id = uuidv4();
+              area.number = counters.area_number++;
+              area.name = "Concourse C North";
+              results.push(area);
+              results = getCells(area, results, counters);
+
+              var area = {};
+              area._class = "area";
+              area.site_id = site._id;          
+              area.region_id = site.region_id;
+              area.country_id = site.country_id;
+              area.province_id = site.province_id;
+              area.location_id = site.location_id;
+              area.latitude = site.latitude;
+              area.longitude = site.longitude;
+              area.has_active = false;
+
+              area._id = uuidv4();
+              area.number = counters.area_number++;
+              area.name = "Concourse C West";
+              results.push(area);
+              results = getCells(area, results, counters);
+
+              var area = {};
+              area._class = "area";
+              area.site_id = site._id;          
+              area.region_id = site.region_id;
+              area.country_id = site.country_id;
+              area.province_id = site.province_id;
+              area.location_id = site.location_id;
+              area.latitude = site.latitude;
+              area.longitude = site.longitude;
+              area.has_active = false;
+
+              area._id = uuidv4();
+              area.number = counters.area_number++;
+              area.name = "Secondary Jetway";
+              results.push(area);
+              results = getCells(area, results, counters);
+
+          var site = {};
+          site._class = "site";
+          site.region_id = element.region_id;
+          site.country_id = element.country_id;
+          site.province_id = element.province_id;
+          site.location_id = element._id;
+          site.has_active = false;
+
+          site._id = uuidv4();
+          site.latitude = element.lat;
+          site.longitude = element.lon;
+          site.name = "Concourse D";
+          results.push(site);
+
+              var area = {};
+              area._class = "area";
+              area.site_id = site._id;          
+              area.region_id = site.region_id;
+              area.country_id = site.country_id;
+              area.province_id = site.province_id;
+              area.location_id = site.location_id;
+              area.latitude = site.latitude;
+              area.longitude = site.longitude;
+              area.has_active = false;
+
+              area._id = uuidv4();
+              area.number = counters.area_number++;
+              area.name = "Concourse D Level 1";
+              results.push(area);
+              results = getCells(area, results, counters);
+
+              var area = {};
+              area._class = "area";
+              area.site_id = site._id;          
+              area.region_id = site.region_id;
+              area.country_id = site.country_id;
+              area.province_id = site.province_id;
+              area.location_id = site.location_id;
+              area.latitude = site.latitude;
+              area.longitude = site.longitude;
+              area.has_active = false;
+
+              area._id = uuidv4();
+              area.number = counters.area_number++;
+              area.name = "Concourse D Level 2";
+              results.push(area);
+              results = getCells(area, results, counters);
+
+              var area = {};
+              area._class = "area";
+              area.site_id = site._id;          
+              area.region_id = site.region_id;
+              area.country_id = site.country_id;
+              area.province_id = site.province_id;
+              area.location_id = site.location_id;
+              area.latitude = site.latitude;
+              area.longitude = site.longitude;
+              area.has_active = false;
+              
+              area._id = uuidv4();
+              area.number = counters.area_number++;
+              area.name = "Concourse D Level 3";
+              results.push(area);
+              results = getCells(area, results, counters);
+
+
+          // throw BreakException;
+        }
+
+    // } catch( e ) {
+    //   console.log('break after ' + ctr + " " + e);
+    //   ctr = 0;
+    // }
+
+  });
+  console.log('write');
+  writeFile(outFile, results);
+}
+
+function getCells(area, results, counters) {
+
+  for (var i = 0; i < 3; i++) {
+    var cell = {};
+    cell._class = "cell";
+    cell._id = uuidv4();
+    cell.number = counters.cell_number++;
+    cell.area_id = area._id;          
+    cell.site_id = area.site_id;          
+    cell.region_id = area.region_id;
+    cell.country_id = area.country_id;
+    cell.province_id = area.province_id;
+    cell.location_id = area.location_id;
+    cell.has_active = false;
+    cell.name = area.number + "." + cell.number;
+    results.push(cell);
+    results = getSensors(area, cell, results, counters);
+  }
+  return results;
+}
+
+function getSensors(area, cell, results, counters) {
+
+  for (var i = 0; i < 3; i++) {
+    var sensor = {};
+    sensor._class = "sensor";
+    sensor._id = uuidv4();
+    sensor.number = counters.sensor_number++;
+    sensor.cell_id = cell._id;
+    sensor.area_id = cell.area_id;          
+    sensor.site_id = cell.site_id;          
+    sensor.region_id = cell.region_id;
+    sensor.country_id = cell.country_id;
+    sensor.province_id = cell.province_id;
+    sensor.location_id = cell.location_id;
+    sensor.has_active = false;
+    sensor.name = area.number + "." + cell.number + "." + sensor.number;
+    results.push(sensor);
+  }
+  return results;
+
+
+}
+
 
 //***********************************
 
